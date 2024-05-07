e86bd38dc0d3719215a6297b14d0209eaebe9023
ridhala <153440834+ridhala@users.noreply.github.com>
2024-05-07 12:15:18 +0200

    Added some Tiwggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,44 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class VinylController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+    }
+
+class VinylController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+        ]);
+    }
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+        //return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
+    }
+}
 class VinylController
 {
     public function homepage()
