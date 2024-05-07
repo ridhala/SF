73900da889bc4a0943762d8cc8c35fe30b9c516a
ridhala <153440834+ridhala@users.noreply.github.com>
2024-05-07 11:49:05 +0200

    route -> controller -> response -> profit

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,6 +1,42 @@
 <?php
-
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    public function homepage()
+    {
+        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
+    }
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+}
+#[Route('/')]
+public function homepage(): Response
+
+use Symfony\Component\Routing\Annotation\Route;
+class VinylController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
+    }
+}
+#[Route('/browse')]
+public function browse(): Response
+{
+    return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
+}
+}
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
