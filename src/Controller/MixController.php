2238e75a56028cdaec0e9f135c823a9285bc1087
ridhala <153440834+ridhala@users.noreply.github.com>
2024-05-14 10:53:30 +0200

    The Request Object

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,14 +6,54 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
 class MixController extends AbstractController
-
-
-    class MixController extends AbstractController
+  
 {
     #[Route('/mix/new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
+        $mix->setTrackCount(rand(5, 20));
+        $mix->setVotes(rand(-50, 50));
+        $entityManager->persist($mix);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Mix %d is %d tracks of pure 80\'s heaven',
+            $mix->getId(),
+            $mix->getTrackCount()
+        ));
+    }
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show(VinylMix $mix): Response
+    {
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
+        #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix): Response
+    {
+        dd($mix);
+    }
+    }
+
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix): Response
+    {
+       
+            $direction = $request->request->get('direction', 'up');
+            if ($direction === 'up') {
+                $mix->setVotes($mix->getVotes() + 1);
+            } else {
+                $mix->setVotes($mix->getVotes() - 1);
+            }
+            dd($mix);
+        }
+    }
+        dd($mix);
+    }
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
@@ -64,7 +104,7 @@ class VinylMixRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-    
+
     * @return VinylMix[] Returns an array of VinylMix objects
     */
    public function findAllOrderedByVotes(): array
