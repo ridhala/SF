aaf0d9e97c52594b04814a3b715d3577ac882b6a
ridhala <153440834+ridhala@users.noreply.github.com>
2024-05-14 12:06:17 +0200

    Simple Doctrine Data Fixtures

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-
 
 class MixController extends AbstractController
 {
@@ -21,25 +21,25 @@ class MixController extends AbstractController
         $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
+
         $entityManager->persist($mix);
         $entityManager->flush();
+
         return new Response(sprintf(
             'Mix %d is %d tracks of pure 80\'s heaven',
             $mix->getId(),
             $mix->getTrackCount()
         ));
     }
-    #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-        if (!$mix) {
-            throw $this->createNotFoundException('Mix not found');
-        }
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
     }
+
     #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
     public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -52,148 +52,7 @@ class MixController extends AbstractController
         $entityManager->flush();
         $this->addFlash('success', 'Vote counted!');
         return $this->redirectToRoute('app_mix_show', [
-            'id' => $mix->getId(),
-        ]);
-    }
-}
-      
-    }
-   
-}
-
-class VinylMixRepository extends ServiceEntityRepository
-{
-
-        parent::__construct($registry, VinylMix::class);
-    }
-    public function add(VinylMix $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-        dd($mix);
-    }
-        $mix = new VinylMix();
-        $mix->setTitle('Do you Remember... Phil Collins?!');
-        $mix->setDescription('A pure mix of drummers turned singers!');
-        $genres = ['pop', 'rock'];
-        $mix->setGenre($genres[array_rand($genres)]);
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(-50, 50));
-        $entityManager->persist($mix);
-        $entityManager->flush();
-        return new Response(sprintf(
-            'Mix %d is %d tracks of pure 80\'s heaven',
-            $mix->getId(),
-            $mix->getTrackCount()
-        ));
-    }
-    
-    }
-
-    public function remove(VinylMix $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    * @return VinylMix[] Returns an array of VinylMix objects
-    */
-   public function findAllOrderedByVotes(): array
-   {
-       return $this->createQueryBuilder('mix')
-           ->orderBy('mix.votes', 'DESC')
-           ->getQuery()
-           ->getResult()
-       ;
-   }
-   public function new(EntityManagerInterface $entityManager): Response
-   {
-       $mix = new VinylMix();
-       $mix->setTitle('Do you Remember... Phil Collins?!');
-       $mix->setDescription('A pure mix of drummers turned singers!');
-       $genres = ['pop', 'rock'];
-       $mix->setGenre($genres[array_rand($genres)]);
-       $mix->setTrackCount(rand(5, 20));
-       $mix->setVotes(rand(-50, 50));
-       $entityManager->persist($mix);
-       $entityManager->flush();
-       return new Response(sprintf(
-           'Mix %d is %d tracks of pure 80\'s heaven',
-           $mix->getId(),
-           $mix->getTrackCount()
-       ));
-   }
-   #[Route('/mix/{id}', name: 'app_mix_show')]
-    
-}
-class VinylMixRepository extends ServiceEntityRepository
-{
-     /**
-     * @return VinylMix[] Returns an array of VinylMix objects
-     */
-    public function findByExampleField($value): array
-    {
-        return $this->createQueryBuilder('v')
-            ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('v.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-     
-    
-    
-
-    private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
-    {
-
-    }
-}
-/**
- * @return VinylMix[] Returns an array of VinylMix objects
- */
-public function findAllOrderedByVotes(string $genre = null): array
-{
-    $queryBuilder = $this->addOrderByVotesQueryBuilder();
-    if ($genre) {
-        $queryBuilder->andWhere('mix.genre = :genre')
-            ->setParameter('genre', $genre);
-    }
-    return $queryBuilder
-        ->getQuery()
-        ->getResult()
-    ;
-}
-private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
-{
-
-    return $queryBuilder->orderBy('mix.votes', 'DESC');
-}
-  public function findOneBySomeField($value): ?VinylMix
-   {
-        return $this->createQueryBuilder('v')
-          ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-           ->getOneOrNullResult()
-       ;
-   }
-}
-    }
-    #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
-    {
-        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
-        return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre,
-            'mixes' => $mixes,
+            'slug' => $mix->getSlug(),
         ]);
     }
 }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,86 +1,15 @@
 <?php
+
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class VinylController extends AbstractController
-{
-    #[Route('/')]
-    public function homepage(): Response
-    {
-        $tracks = [
-            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
-            ['song' => 'Waterfalls', 'artist' => 'TLC'],
-            ['song' => 'Creep', 'artist' => 'Radiohead'],
-            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
-        ];
-    }
-
-class VinylController extends AbstractController
-{
-    #[Route('/')]
-    public function homepage(): Response
-    {
-        return $this->render('vinyl/homepage.html.twig', [
-            'title' => 'PB & Jams',
-        ]);
-    }
-    #[Route('/browse/{slug}')]
-    public function browse(string $slug = null): Response
-    {
-        if ($slug) {
-            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        }
-        return new Response($title);
-        //return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
-    }
-}
-class VinylController
-{
-    public function homepage()
-    {
-        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
-    }
-    #[Route('/browse/{slug}')]
-    public function browse(string $slug = null): Response
-    {
-        if ($slug) {
-            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        }
-        return new Response($title);
-}
-#[Route('/')]
-public function homepage(): Response
-
-use Symfony\Component\Routing\Annotation\Route;
-class VinylController
-{
-    #[Route('/')]
-    public function homepage()
-    {
-        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
-    }
-}
-#[Route('/browse')]
-public function browse(): Response
-{
-    return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
-}
-}
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Pagerfanta\Pagerfanta;
+use App\Repository\VinylMixRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use function Symfony\Component\String\u;
-
-
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class VinylController extends AbstractController
@@ -97,18 +26,24 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
         return $this->render('vinyl/homepage.html.twig', [
-            'title' => 'ISET Kélibia',
+            'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
     }
     #[Route('/browse/{slug}', name: 'app_browse')]
-    function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, Request $request, string $slug = null): Response
     {
-
-        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
-
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $queryBuilder = $mixRepository->createOrderedByVotesQueryBuilder($slug);
+        $adapter = new QueryAdapter($queryBuilder);
+        $pagerfanta = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            $adapter,
+            $request->query->get('page', 1),
+            9
+        );
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
+            'pager' => $pagerfanta,
         ]);
     }
 }
