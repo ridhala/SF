1ca8122e65007d7d6141eadf17b4f79370cd1e76
ridhala <153440834+ridhala@users.noreply.github.com>
2024-05-14 11:33:18 +0200

    Updating an Entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -13,47 +13,9 @@ class MixController extends AbstractController
   
 {
     #[Route('/mix/new')]
+    #[Route('/mix/new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(-50, 50));
-        $entityManager->persist($mix);
-        $entityManager->flush();
-        return new Response(sprintf(
-            'Mix %d is %d tracks of pure 80\'s heaven',
-            $mix->getId(),
-            $mix->getTrackCount()
-        ));
-    }
-    #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show(VinylMix $mix): Response
-    {
-        return $this->render('mix/show.html.twig', [
-            'mix' => $mix,
-        ]);
-        #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
-    public function vote(VinylMix $mix): Response
-    {
-        dd($mix);
-    }
-    }
-
-
-    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
-    public function vote(VinylMix $mix): Response
-    {
-       
-            $direction = $request->request->get('direction', 'up');
-            if ($direction === 'up') {
-                $mix->setVotes($mix->getVotes() + 1);
-            } else {
-                $mix->setVotes($mix->getVotes() - 1);
-            }
-            dd($mix);
-        }
-    }
-        dd($mix);
-    }
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
@@ -69,7 +31,27 @@ class MixController extends AbstractController
             $mix->getTrackCount()
         ));
     }
-    
+
+   
+
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
+        ]);
+    }
+}
+    }
+      
     }
     #[Route('/mix/{id}', name: 'app_mix_show')]
     public function show($id, VinylMixRepository $mixRepository): Response
@@ -95,6 +77,24 @@ class VinylMixRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+        dd($mix);
+    }
+        $mix = new VinylMix();
+        $mix->setTitle('Do you Remember... Phil Collins?!');
+        $mix->setDescription('A pure mix of drummers turned singers!');
+        $genres = ['pop', 'rock'];
+        $mix->setGenre($genres[array_rand($genres)]);
+        $mix->setTrackCount(rand(5, 20));
+        $mix->setVotes(rand(-50, 50));
+        $entityManager->persist($mix);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Mix %d is %d tracks of pure 80\'s heaven',
+            $mix->getId(),
+            $mix->getTrackCount()
+        ));
+    }
+    
     }
 
     public function remove(VinylMix $entity, bool $flush = false): void
